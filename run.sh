ad779f5582829871738daa114a56040908b696f0
Scott Ouellette <scottx611x@gmail.com>
2018-03-24 16:58:54 -0400

    Remove blank line

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ DEFAULTS="--volume="$PWD/data:/srv/jekyll" \
   		  --volume="$PWD/data/vendor/bundle:/usr/local/bundle" \
           -it jekyll/jekyll:$JEKYLL_VERSION"
 
-
 rm -rf $PWD/data/_site
 
 docker run --rm -d -p 4000:4000 $DEFAULTS jekyll serve

a2111fa0cacc11b2b263dffb4fca90749938ce0a
Scott Ouellette <scottx611x@gmail.com>
2018-03-24 17:16:43 -0400

    Just build site on Travis; Serve it locally

--- run.sh
+++ run.sh
@@ -6,4 +6,8 @@ DEFAULTS="--volume="$PWD/data:/srv/jekyll" \
 
 rm -rf $PWD/data/_site
 
-docker run --rm -d -p 4000:4000 $DEFAULTS jekyll serve
+if [ "$CONTINUOUS_INTEGRATION" = "true"]; 
+	then docker run --rm $DEFAULTS jekyll build; # Just build on Travis
+else
+	docker run --rm -d -p 4000:4000 $DEFAULTS jekyll serve # Serve content for local dev
+fi

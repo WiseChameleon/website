9859bef58876e25be00a06f6f7d2f410f6290893
Scott Ouellette <scottx611x@gmail.com>
2018-03-24 17:21:12 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 export JEKYLL_VERSION=3.5
 
-DEFAULTS="--volume="$PWD/data:/srv/jekyll" \
+DEFAULTS="--rm \
+		  --volume="$PWD/data:/srv/jekyll" \
   		  --volume="$PWD/data/vendor/bundle:/usr/local/bundle" \
           -it jekyll/jekyll:$JEKYLL_VERSION"
 
 rm -rf $PWD/data/_site
 
 if [ "$CONTINUOUS_INTEGRATION" = "true"]; 
-	then docker run --rm $DEFAULTS jekyll build; # Just build on Travis
+	then docker run $DEFAULTS jekyll build; # Just build on Travis
+	sleep 30
 else
-	docker run --rm -d -p 4000:4000 $DEFAULTS jekyll serve # Serve content for local dev
+	docker run -d -p 4000:4000 $DEFAULTS jekyll serve # Serve content for local dev
 fi
